2faa7879a99f290ee8a71d5e21bc2077367a3a90
kumarsyadav2 <77455527+kumarsyadav2@users.noreply.github.com>
2023-04-15 23:54:52 +0530

    Time: 213 ms (86.28%), Space: 133 MB (29.87%) - LeetHub

--- 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.cpp
+++ 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.cpp
@@ -1,29 +1,29 @@
-class Solution {
+class Solution
+{
 public:
-    vector<int> pivotArray(vector<int>& nums, int pivot) {
-        int n = nums.size();
-        vector<int>small;
-        vector<int>large;
-        vector<int>equal;
-        for(int i=0;i<n;i++){
-            if(nums[i]<pivot) small.push_back(nums[i]);
-            if(nums[i]==pivot) equal.push_back(nums[i]);
-            if(nums[i]>pivot) large.push_back(nums[i]);
-        }
-        
-        int s = small.size();
-        int e = equal.size();
-        int l = large.size();
-        
-        for(int i=0;i<s;i++){
-            swap(nums[i],small[i]);
-        }
-        for(int i=0;i<e;i++){
-            swap(nums[i+s],equal[i]);
-        }
-        for(int i=0;i<l;i++){
-            swap(nums[i+s+e],large[i]);
-        }
-        return nums;
-    }
+	vector<int> pivotArray(vector<int> &nums, int pivot)
+	{
+		vector<int> store1;
+		vector<int> store2;
+
+		int n = nums.size();
+		int count = 0;
+
+		for (int i = 0; i < n; i++)
+		{
+			if (nums[i] < pivot)
+				store1.push_back(nums[i]);
+			else if (nums[i] == pivot)
+				count++;
+			else
+				store2.push_back(nums[i]);
+		}
+
+		for (int i = 0; i < count; i++)
+			store1.push_back(pivot);
+        for(int i=0;i<store2.size();i++)
+	    store1.push_back(store2[i]);
+
+		return store1;
+	}
 };
